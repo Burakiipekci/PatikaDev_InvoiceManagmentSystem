7296912b0171979adc0843acc3af345e55ac5406
Burak İpekci <burakipekci@outlook.com>
2023-03-02 14:34:12 +0300

    Block bussines code  message bussiness code added

---
+++ InvoiceManagmentSystem.Business/Abstract/IBlockService.cs
@@ -0,0 +1,18 @@
+﻿using InvoiceManagmentSystem.Core.Utilities.Results;
+using InvoiceManagmentSystem.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagmentSystem.Business.Abstract
+{
+    public interface IBlockService
+    {
+        IResult Add(Block block);
+        IResult Update(Block block);
+        IResult Delete(int id);
+        IDataResult<List<Block>> GetAll();
+    }
+}
